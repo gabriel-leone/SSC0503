e9a45b6188dc9c0d716d98fc5ad9bf8cc236e073
gabriel-leone <leonepgabriel@gmail.com>
2023-08-29 10:37:05 -0300

    fix atividade 1

--- Atividade1/sort.c
+++ Atividade1/sort.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-void swap(int *endereco1, int *endereco2, int index1, int index2)
-{
-  printf("T %d %d\n", index1, index2);
-  int temp = *endereco1;
-  *endereco1 = *endereco2;
-  *endereco2 = temp;
+void printTroca(int index1, int index2) {
+    printf("T %d %d\n", index1, index2);
+}
+
+void printComparacao(int index1, int index2) {
+    printf("C %d %d\n", index1, index2);
 }
 
 void bubble(int array[], int arraySize)
 {
-  int i = arraySize - 1;
-
-  while (i >= 1)
+  int mark;
+  for (int i = arraySize - 1; i >= 1; i = mark)
   {
+    mark = -1;
     for (int j = 0; j < i; j++)
     {
-      printf("C %d %d\n", j, j + 1);
+      printComparacao(j, j + 1 );
       if (array[j] > array[j + 1])
       {
-        // printf("T %d %d\n", j, j + 1);
-        // int tmp = array[j];
-        // array[j] = array[j + 1];
-        // array[j + 1] = tmp;
-        swap(&array[j], &array[j + 1], j, j + 1);
+        printTroca(j, j + 1);
+        mark = j;
+        int tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
       }
     }
-    i--;
   }
 }
 
@@ -36,46 +35,55 @@ void selection(int array[], int arraySize)
   for (int i = 0; i < arraySize - 1; i++)
   {
     int m = i;
-
     for (int j = i + 1; j < arraySize; j++)
     {
-      printf("C %d %d\n", m, j);
+      printComparacao(m, j);
       if (array[j] < array[m])
       {
         m = j;
       }
     }
-    if (m == i)
-      continue;
-    swap(&array[m], &array[i], i, m);
+
+    if (m != i)
+    {
+      printTroca(i, m);
+      int tmp = array[i];
+      array[i] = array[m];
+      array[m] = tmp;
+    }
   }
 }
 
 int main()
 {
   // Define o método de ordenação
-  char metodoOrdenacao[10];
-  scanf("%s", metodoOrdenacao);
-  char bolha[10] = "bolha";
-  char selecao[10] = "selecao";
+  char metodo[100];
+  scanf("%s", metodo);
 
-  // Define o tamanho do array e faz a leitura
+  // Lê o tamanho do array
   int arraySize;
   scanf("%d", &arraySize);
   int array[arraySize];
+
+  // Lê os elementos do array
   for (int i = 0; i < arraySize; i++)
   {
     scanf("%d", &array[i]);
   }
 
   // Chama a função de ordenação
-  if (strcmp(metodoOrdenacao, bolha) == 0)
+  if (strcmp(metodo, "selecao") == 0)
+  {
+    selection(array, arraySize);
+  }
+  else if (strcmp(metodo, "bolha") == 0)
   {
     bubble(array, arraySize);
   }
-  else if (strcmp(metodoOrdenacao, selecao) == 0)
+  else
   {
-    selection(array, arraySize);
+    printf("Metodo de ordenacao invalido.\n");
+    return 1;
   }
 
   // Imprime o array ordenado
